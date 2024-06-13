a4c5def096c0fa9703920bb6be2ad20190c6e17a
Saravanak Karthick <sara.karth158@gmail.com>
2024-06-13 09:05:01 +0530

    first app-view-polls

--- my_new_django_server/settings.py
+++ my_new_django_server/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,7 +24,8 @@ SECRET_KEY = 'django-insecure-g+qbi%ey$@7x8s!zt!wxe&w(t-m*eg*%7pbbo2!8rr)h&j!69s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'polls'
 ]
 
 MIDDLEWARE = [
---
+++ polls/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Add this line
+    #path('index/', views.index, name='index'),  # Assuming you have an index view
+]
