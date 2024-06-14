1379160a0f3ef44e8a676d2de52069f709440a00
Saravanak Karthick <sara.karth158@gmail.com>
2024-06-14 10:02:51 +0530

    14-06-24

--- my_new_django_server/urls.py
+++ my_new_django_server/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("polls/home", include("polls.urls")),  # Change "polls/" to ""
+    path("", include("polls.urls")),  # Change "polls/" to ""
     
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+
     return HttpResponse("Welcome to the home page!")
+    
