1f68db59d6bfaddcd16fa582888e13292794d53d
Saravanak Karthick <sara.karth158@gmail.com>
2024-06-16 22:41:07 +0530

    views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('hello.html', views.home, name='home'),  # Add this line
-    #path('index/', views.index, name='index'),  # Assuming you have an index view
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    #path("specifics/<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
